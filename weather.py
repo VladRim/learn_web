afbabd371a703447fb0150b217933130900af50c
admsys <vlad_rym@mail.ru>
2020-02-27 17:07:37 +0300

    firs draft web-app

---
+++ weather.py
@@ -0,0 +1,25 @@
+import requests
+
+def weather_by_city(city_name):
+    weather_url = "http://api.worldweatheronline.com/premium/v1/weather.ashx"
+    params = {
+        "key": "7369628b815b4363829134908202702",
+        "q": city_name,
+        "format": "json",
+        "num_of_days": 1,
+        "lang": "ru"
+    }
+    result = requests.get(weather_url, params=params)
+    return result.json()
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+	print(weather_by_city("Moscow, Russia"))
